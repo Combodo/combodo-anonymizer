ec4dec11c7459de4401bfa7d7f658e0d6d6f6407
Stephen Abello <stephen.abello@combodo.com>
2019-06-14 11:38:06 +0200

    Update module version to 1.0.1

--- module.combodo-anonymizer.php
+++ module.combodo-anonymizer.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-anonymizer/1.0.0',
+	'combodo-anonymizer/1.0.1',
 	array(
 		// Identification
 		//
