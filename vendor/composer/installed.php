7a19c5ae7b676275a03881fd50334c59d0f4fbb2
Eric Espie <eric.espie@combodo.com>
2022-10-31 15:03:33 +0100

    N°5400 - removed composer PHP Version control

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,23 +1,23 @@
 <?php return array(
-	'root'     => array(
-		'pretty_version' => 'dev-master',
-		'version'        => 'dev-master',
-		'type'           => 'itop-extension',
-		'install_path'   => __DIR__.'/../../',
-		'aliases'        => array(),
-		'reference'      => '88586ac903b137a26cf176e841dec4c714010862',
-		'name'           => 'combodo/combodo-anonymizer',
-		'dev'            => true,
-	),
-	'versions' => array(
-		'combodo/combodo-anonymizer' => array(
-			'pretty_version'  => 'dev-master',
-			'version'         => 'dev-master',
-			'type'            => 'itop-extension',
-			'install_path'    => __DIR__.'/../../',
-			'aliases'         => array(),
-			'reference'       => '88586ac903b137a26cf176e841dec4c714010862',
-			'dev_requirement' => false,
-		),
-	),
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'itop-extension',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '5f8212d98efa6611d98ce2c71be83fb6eaf05c33',
+        'name' => 'combodo/combodo-anonymizer',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'combodo/combodo-anonymizer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'itop-extension',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '5f8212d98efa6611d98ce2c71be83fb6eaf05c33',
+            'dev_requirement' => false,
+        ),
+    ),
 );
